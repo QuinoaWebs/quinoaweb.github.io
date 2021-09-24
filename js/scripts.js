69c263c5ce02047dfbfa394515af63bac917b95a
joaquin <jpascual03380@gmail.com>
2021-09-24 13:32:49 +0200

    primer commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const mainNav = document.getElementById('main-nav');
+const toggleMenu = document.getElementById('toggle-menu');
+const toggleBar = document.getElementById('toggle-bar');
+const headerElement = document.getElementById('header');
+const mainElement = document.getElementById('main');
+
+
+toggleMenu.addEventListener('click', () => {
+  toggleBar.classList.toggle('active');
+  mainNav.classList.toggle('main-nav--show');
+  header.classList.toggle('blur');
+  main.classList.toggle('blur');
+  document.body.classList.toggle('no-scroll');
+});
+
+mainNav.addEventListener("click", (e)=> {
+ if(e.target.classList.contains('main-menu__link')){
+
+  toggleBar.classList.remove("active"),
+  mainNav.classList.remove("main-nav--show"),
+  header.classList.remove("blur"),
+  main.classList.remove("blur"),
+  document.body.classList.toggle("no-scroll");
+ }
+
+});
